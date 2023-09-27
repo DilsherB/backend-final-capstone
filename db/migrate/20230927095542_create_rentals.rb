d0c12e33527b1da346b2695abf56cca38dc65f85
Dilsher Balouch <dilsherb@gmail.com>
2023-09-27 12:59:55 +0300

    rental model created.

---
+++ db/migrate/20230927095542_create_rentals.rb
@@ -0,0 +1,17 @@
+class CreateRentals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rentals do |t|
+      t.integer :rental_number
+      t.date :rental_date
+      t.datetime :time_depart
+      t.datetime :time_arrive
+      t.string :destination
+      t.date :date_return
+      t.float :payment
+      t.references :user, null: false, foreign_key: true
+      t.references :car, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
