139e862d4f0747bcbf6935f3196b42869e99d0da
Fezzopro <fezzopro@gmail.com>
2023-09-29 12:20:45 +0200

    Add rack middleware to application configurations

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module BackendFinalCapstone
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      end
+    end
   end
 end
