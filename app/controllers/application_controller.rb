0b574010ba44fe733cce22f51f881c0b2ebfe4ea
Fezzopro <fezzopro@gmail.com>
2023-09-29 10:21:18 +0200

    Add user registration controler action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up) do |params|
+      params.permit(%i[email password password_confirmation phone_number name])
+    end
+  end
 end
