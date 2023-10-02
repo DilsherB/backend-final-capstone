d33b464991c6d028c84a73ff675c5f029ae86825
Dilsher Balouch <dilsherb@gmail.com>
2023-10-02 16:06:29 +0300

    create controller of rental is written.

--- app/controllers/rental_controller.rb
+++ app/controllers/rental_controller.rb
@@ -6,4 +6,13 @@ class RentalController < ApplicationController
   def show
     render json: Rental.find(params[:id])
   end
+
+  def create
+    rental = Rental.new(rental_params)
+    if rental.save
+      render json: rental, status: :created
+    else
+      render json: rental.errors, status: :unprocessable_entity
+    end
+  end
 end
