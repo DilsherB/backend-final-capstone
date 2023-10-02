a559fb96cbcdd5d57c01bd473a6156d162064d13
Dilsher Balouch <dilsherb@gmail.com>
2023-10-02 16:14:12 +0300

    rental params defined.

--- app/controllers/rental_controller.rb
+++ app/controllers/rental_controller.rb
@@ -23,4 +23,10 @@ class RentalController < ApplicationController
   def destroy
     render json: Rental.destroy(params[:id])
   end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:rental_number, :rental_date, :time_depart, :time_arrive, :date_return)
+  end
 end
