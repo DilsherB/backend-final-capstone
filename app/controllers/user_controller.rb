58406bcfa1086468685df27a3e236b69ec0d0e48
Dilsher Balouch <dilsherb@gmail.com>
2023-10-02 16:22:18 +0300

    create controller for user has been written

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,13 @@ class UserController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
 end
