130e3953685ede505688ff3cbc405ff3559e041f
Fezzopro <fezzopro@gmail.com>
2023-10-02 14:06:08 +0200

    Update Car Model Validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,8 @@ class Car < ApplicationRecord
   belongs_to :model
   has_many :rentals, foreign_key: 'car_id'
 
-  validates :license_plate, presence: true, uniqueness: true
-  validates :status, :name, :image, :price, presence: true
+  validates :plate_number, presence: true, uniqueness: true
+  validates :image, :price, presence: true
   validates :image, format: { with: /\.(png|jpg|jpeg)\Z/i, message: 'must be a URL for GIF, JPG or PNG image.' }
+  validates_associated :model
 end
