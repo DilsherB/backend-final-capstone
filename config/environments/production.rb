1cbc5879a09c20a9f38c174295414701e2297941
Fezzopro <fezzopro@gmail.com>
2023-10-05 20:40:12 +0200

    Change production version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.routes.default_url_options = { host: ENV.fetch('HOST'), port: ENV.fetch('PORT') }
+Rails.application.routes.default_url_options = { host: ENV.fetch('HOST') }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
