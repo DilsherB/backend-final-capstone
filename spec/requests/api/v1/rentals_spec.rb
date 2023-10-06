d7ba861148250d0510420f6676f54157c6758bed
Racha OULDBABAALI <rachaould@gmail.com>
2023-10-06 10:31:22 +0100

    Fix rubocop

--- spec/requests/api/v1/rentals_spec.rb
+++ spec/requests/api/v1/rentals_spec.rb
@@ -13,21 +13,25 @@ RSpec.describe Api::V1::RentalsController, type: :controller do
   end
 
   describe 'POST #create' do
-    let(:valid_params) { { rental_date: Date.today, destination: 'Sample', date_return: Date.tomorrow, car_id: car.id, user_id: user.id } }
+    let(:valid_params) do
+      { rental_date: Date.today, destination: 'Sample', date_return: Date.tomorrow, car_id: car.id, user_id: user.id }
+    end
 
     it 'creates a new rental' do
-      expect {
+      expect do
         post :create, params: valid_params
-      }.to change(Rental, :count).by(0)
+      end.to change(Rental, :count).by(0)
       expect(response).to have_http_status(:unauthorized)
     end
   end
 
   describe 'PUT #update' do
-    let(:rental) { create(:rental, user: user) }
+    let(:rental) { create(:rental, user:) }
 
     it 'updates the rental' do
-      put :update, params: { id: rental.id, user_id: user.id, car_id: car.id, rental_date: Date.today, destination: 'Langoshtown' }
+      put :update,
+          params: { id: rental.id, user_id: user.id, car_id: car.id, rental_date: Date.today,
+                    destination: 'Langoshtown' }
       expect(response).to have_http_status(:unauthorized)
       rental.reload
       expect(rental.destination).to eq('Langoshtown')
@@ -35,11 +39,11 @@ RSpec.describe Api::V1::RentalsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    let!(:rental) { create(:rental, user: user) }
+    let!(:rental) { create(:rental, user:) }
     it 'destroys the rental' do
-      expect {
+      expect do
         delete :destroy, params: { id: rental.id }
-      }.to change(Rental, :count).by(0)
+      end.to change(Rental, :count).by(0)
       expect(response).to have_http_status(:unauthorized)
     end
   end
